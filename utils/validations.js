70b3a4322ce5fb5abbeae7ea866e7bb863ada6d9
Mar. Leska <mariales.leska@gmail.com>
2025-06-28 19:43:49 +0300

    Add password validation

--- utils/validations.js
+++ utils/validations.js
@@ -1,13 +1,12 @@
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-const isEmailFormatCorrect = (email) => {
+exports.emailFormat = (email) => {
   return emailRegex.test(email);
 }
 
-// const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-// if (!passwordRegex.test(password)) {
-//   throw new apiError(400, "Password must be at least 8 characters long and include at least one letter and one number");
-// }
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$]).{8,}$/;
 
+exports.passwordFormat = (password) => {
+  return passwordRegex.test(password)
+}
 
-module.exports = isEmailFormatCorrect
