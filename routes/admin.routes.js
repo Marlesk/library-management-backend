021c53cf29a8adc57418990d792a127bb7d3116d
Mar. Leska <mariales.leska@gmail.com>
2025-06-28 19:41:31 +0300

    Changes routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -4,14 +4,24 @@ const router = express.Router()
 const verifyRole = require('../middlewares/auth.middleware').verifyRole
 const verifyToken = require('../middlewares/auth.middleware').verifyToken
 const adminController = require('../controllers/admin.controller')
+const bookController = require('../controllers/book.controller')
 
+
+// Manage Users
 router.get('/users', verifyToken, verifyRole('admin'), adminController.getAllUsers)
-
 router.get('/users/username/:username', verifyToken, verifyRole('admin'), adminController.searchByUsername)
 router.get('/users/email/:email', verifyToken, verifyRole('admin'), adminController.searchByEmail)
 
 router.delete('/users/username/:username', verifyToken, verifyRole('admin'), adminController.deleteByUsername)
-
 router.delete('/users/email/:email', verifyToken, verifyRole('admin'), adminController.deleteByEmail)
+
+// Manage Books
+router.get('/books/google-search/:title/:author', verifyToken, verifyRole('admin'), bookController.searchBook)
+
+router.post('/books', verifyToken, verifyRole('admin'), bookController.addBook)
+
+router.patch('/books/:isbn', verifyToken, verifyRole('admin'), bookController.updateBookByIsbn)
+
+router.delete('/books/:isbn', verifyToken, verifyRole('admin'), bookController.deleteByIsbn)
 
 module.exports = router
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,4 +5,9 @@ const authController = require('../controllers/auth.controller')
 
 router.post('/login', authController.login)
 
+router.get('/google/callback', authController.googleLogin)
+
 module.exports = router
+
+
+// https://accounts.google.com/o/oauth2/auth?client_id=773356764549-lhmmob9gs839q0h577g99nflt87kkjkd.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
