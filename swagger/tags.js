c5c3a217a9ef392cdaf44a7557d6836a9a8d254f
Mar. Leska <mariales.leska@gmail.com>
2025-07-05 19:12:43 +0300

    Add new tags for borrows

--- swagger/tags.js
+++ swagger/tags.js
@@ -3,22 +3,38 @@ module.exports = [
     "name": "Users",
     "description": "Endpoints for User"
   },
-  {
-    "name": "Admin-Users",
-    "description": "Endpoints for managing users (Admin only)"
-  },
-  {
-    "name": "Admin-Books",
-    "description": "Endpoints for managing books (Admin only)"
-  },
+
   {
     "name": "Books",
     "description": "Endpoints for Book"
   },
+
+  {
+    "name": "Borrow",
+    "description": "Endpoints for Borrow"
+  },
+
+  {
+    "name": "Admin-Users",
+    "description": "Endpoints for managing users (Admin only)"
+  },
+
+  {
+    "name": "Admin-Books",
+    "description": "Endpoints for managing books (Admin only)"
+  },
+
+  {
+    "name": "Admin-Borrows",
+    "description": "Endpoints for managing borrowed books (Admin only)"
+  },
+
+
   {
     "name": "Auth",
     "description": "Endpoints for Authentication"
   },
+  
   {
     "name": "Google Login/Register",
     "description": "Endpoints for Google Authentication"
