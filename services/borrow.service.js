fc3edab7ba3b7ed4efe63272d30c9fcd24367402
Mar. Leska <mariales.leska@gmail.com>
2025-07-05 19:08:53 +0300

    Add borrow services

---
+++ services/borrow.service.js
@@ -0,0 +1,138 @@
+const Borrow = require('../models/borrow.model')
+const Book = require('../models/book.model')
+const apiError = require('../utils/ApiErrors')
+
+
+// For Users
+exports.findBookByIsbn = async(isbn) => {
+  return await Book.findOne({ isbn })
+}
+
+exports.findBorrowBook = async(userId) => {
+  return await Borrow.findOne( { userId, status: { $in: ['requested', 'borrowed'] } } )
+}
+
+exports.requestBook = async(userId, isbn) => {
+
+  if (!isbn) {
+    throw new apiError(400, 'ISBN is a required field')
+  }
+
+  const book = await this.findBookByIsbn(isbn)
+  if (!book) {
+    throw new apiError(404, 'Book not found')
+  }
+
+  if (!book.available) {
+    throw new apiError(400, 'This book is currently unavailable (already borrowed)')
+  }
+
+  const existingBorrow = await this.findBorrowBook(userId)
+  if (existingBorrow) {
+    if (existingBorrow.status === 'requested') {
+      throw new apiError(400, 'You already have a pending borrow request');
+    }
+    if (existingBorrow.status === 'borrowed') {
+      throw new apiError(400, 'You already have a borrowed book. Return it before requesting another');
+    }
+  }
+
+  const timestamp = Date.now().toString().slice(-6)
+  const random = Math.floor(100 + Math.random() * 900)
+  const borrowCode = `BRC-${timestamp}${random}`
+
+  const borrow = new Borrow({
+    userId: userId,
+    bookId: book._id,
+    borrowCode: borrowCode,
+    status: 'requested',
+    borrowDate: null,
+    returnDate: null,
+  })
+
+  const saved = await borrow.save()
+
+  book.available = false
+  await book.save()
+
+  return { borrowCode, book }
+
+}
+
+exports.getBorrowBooks = async(userId, status) => {
+  const query = { userId }
+
+  if (status === 'requested') query.status = 'requested'
+  else if (status === 'borrowed') query.status = 'borrowed'
+  else if (status === 'returned') query.status = 'returned'
+
+  const records = await Borrow.find(query).populate('bookId')
+  if (!records || records.length === 0) {
+    throw new apiError(404, `No ${status || ''} records found`)
+  }
+
+  return records
+}
+
+
+// For Admin
+exports.getAllRecordsBooks = async (status) => {
+  const query = {}
+
+  if (status === 'requested') query.status = 'requested'
+  else if (status === 'borrowed') query.status = 'borrowed'
+  else if (status === 'returned') query.status = 'returned'
+
+  return await Borrow.find(query).populate('bookId')
+          .populate({path: 'userId', select: 'firstname lastname email'})
+}
+
+// Accept a borrow request (change status to borrowed)
+exports.acceptRequest = async (code) => {
+  if (!code) {
+    throw new apiError(400, 'Borrow Code is a required field')
+  }
+
+  const borrow = await Borrow.findOne({borrowCode: code, status: 'requested'}).populate('bookId')
+                                          .populate({path: 'userId', select: 'firstname lastname email'})
+  if (!borrow) {
+    throw new apiError(404, 'No pending borrow request found for the given code')
+  }
+  
+  borrow.status = 'borrowed'
+  borrow.borrowDate = new Date()
+  await borrow.save()
+
+  return borrow
+}
+
+// Register book return
+exports.returnBook = async (isbn) => {
+  if (!isbn) {
+    throw new apiError(400, 'ISBN is a required field')
+  }
+
+  const book = await Book.findOne({isbn})
+  if (!book) {
+    throw new apiError(404, 'Book not found')
+  }
+
+  const borrow = await Borrow.findOne({bookId: book._id, status: 'borrowed'}).populate('bookId')
+                                  .populate({path: 'userId', select: 'firstname lastname email'})
+  if (!borrow) {
+    throw new apiError(400, 'No active borrow record found for this book')
+  } 
+  
+  borrow.status = 'returned'
+  borrow.returnDate = new Date()
+  await borrow.save()
+
+  book.available = true
+  await book.save()
+
+  return borrow
+}
+
+
+
+
