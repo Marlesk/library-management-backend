4f2c4f01a8f46fa29c59fb8a2b719180e8c7d109
Mar. Leska <mariales.leska@gmail.com>
2025-07-27 18:40:27 +0300

    Add contact schema

--- swagger/components/schemas.js
+++ swagger/components/schemas.js
@@ -2,9 +2,11 @@ const m2s = require('mongoose-to-swagger')
 const User = require('../../models/user.model')
 const Book = require('../../models/book.model')
 const Borrow = require('../../models/borrow.model')
+const Contact = require('../../models/contact.model')
 
 module.exports = {
   User: m2s(User),
   Book: m2s(Book),
-  Borrow: m2s(Borrow)
+  Borrow: m2s(Borrow),
+  Contact: m2s(Contact)
 }
