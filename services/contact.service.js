5c5193ccab83ca7ea88d86fccb853aa0f0379ba4
Mar. Leska <mariales.leska@gmail.com>
2025-07-27 18:39:47 +0300

    Add contact services

---
+++ services/contact.service.js
@@ -0,0 +1,30 @@
+const Contact = require('../models/contact.model')
+const apiError = require('../utils/ApiErrors')
+const validations = require('../utils/validations')
+
+exports.sendMessage = async(data) => {
+  // Έλεγχος για required πεδία
+    for (let key in data) {
+      if (!data[key]) {
+        throw new apiError(400, `${key} is required field`)
+      }
+    }
+  
+    // Έλεγχος για email validation
+    if (!validations.emailFormat(data.email)) {
+      throw new apiError(400, "Email format is not correct.");
+    }
+
+    const newContact = new Contact({
+      firstname: data.firstname,
+      lastname: data.lastname,
+      email: data.email,
+      message: data.message
+    })
+
+  return await newContact.save()
+}
+
+exports.getMessage = async() => {
+  return Contact.find().sort({ createdAt : -1})
+}
