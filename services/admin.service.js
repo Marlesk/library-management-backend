f43359a3dca8a44343f49630b23f887c44d0ab45
Mar. Leska <mariales.leska@gmail.com>
2025-08-17 15:49:29 +0300

    change output from find

--- services/admin.service.js
+++ services/admin.service.js
@@ -1,22 +1,24 @@
 const User = require('../models/user.model')
 const validations = require('../utils/validations')
-const apiError = require('../utils/ApiErrors')
+const ApiError = require('../utils/ApiErrors')
 
 exports.findAllUsers = async() => {
-  return await User.find().select('-password')
+  return await User.find()
+                  .select('-_id firstname lastname email username role createdAt')
+                  .sort({ createdAt : -1})
 }
 
 exports.findByUsername = async(username) => {
-  return await User.findOne({username: username}).select('-password')
+  return await User.findOne({username: username}).select('-_id firstname lastname email username createdAt')
 }
 
 exports.findByEmail = async(email) => {
   // Έλεγχος για email validation
   if (!validations.emailFormat(email)) {
-    throw new apiError(400, "Email format is not correct.");
+    throw new ApiError(400, "Email format is not correct.");
   }
 
-  return await User.findOne({email: email}).select('-password')
+  return await User.findOne({email: email}).select('-_id firstname lastname email username createdAt')
 }
 
 exports.findByUsernameAndDelete = async(username) => {
