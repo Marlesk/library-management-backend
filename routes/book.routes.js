fdc2fc590440e81656d034007ca234ac877fc61d
Mar. Leska <mariales.leska@gmail.com>
2025-08-17 15:48:09 +0300

    add new route

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -4,7 +4,8 @@ const bookController = require('../controllers/book.controller')
 const verifyToken = require('../middlewares/auth.middleware').verifyToken
 
 router.get('/', verifyToken, bookController.getAllBooks)
-router.get('/title/:title', verifyToken,  bookController.getBookByTitle)
+router.get('/title/:title', verifyToken,  bookController.getBooksByTitle)
 router.get('/author/:author', verifyToken, bookController.getBooksByAuthor)
+router.get('/isbn/:isbn', verifyToken, bookController.getBookByIsbn)
 
 module.exports = router 
